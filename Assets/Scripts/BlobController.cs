943c1169e77275fc327632f62c6065674bb444f3
TristanStreich <tristanstreich@gmail.com>
2023-01-14 10:19:11 -0800

    verticalScrollController -> blobController

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class VerticalScrollController : MonoBehaviour
+public class BlobController : MonoBehaviour
 {
     public static event Action JumpingEvent;
     public static float jumpForce;
