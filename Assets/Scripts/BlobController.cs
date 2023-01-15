7aa5cdc4a0f524a8e75d488954ad2f67fb00b736
TristanStreich <tristanstreich@gmail.com>
2023-01-15 10:56:11 -0800

    custom gravity system for blob. Faster fall

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -6,36 +6,36 @@ using UnityEngine.Events;
 
 public class BlobController : MonoBehaviour
 {
-    public static event Action JumpingEvent;
+    public static event Action JumpingEvent; //TODO: probably remove this
     public float jumpHoldDuration = 10_000f;
     public float moveSpeed;
     public float jumpForce = 4f;
 
 
-    private bool isGrounded;
+    private bool isGrounded; //TODO: add checking ground to stop air jumping
     private bool isHoldingJump;
     private float jumpStartTime;
 
 
     private Rigidbody rb;
     
-    void Awake()
-    {
+    void Start() {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per render frame
     void Update()
     {
-        horizontalMovementSystem();
+        HorizontalMovementSystem();
     }
 
     // called once per physics frame
     void FixedUpdate() {
-        HandleJumpingInputs();
+        JumpingSystem();
+        FastFallGravitySystem();
     }
 
-    void horizontalMovementSystem() {
+    void HorizontalMovementSystem() {
         // Get input from the horizontal and vertical axes
         float horizontalInput = Input.GetAxis("Horizontal");
 
@@ -50,7 +50,7 @@ public class BlobController : MonoBehaviour
         rb.velocity = speed * Vector3.up;
     }
 
-    public void HandleJumpingInputs() {
+    public void JumpingSystem() {
         // Do not accept jumping inputs if not in the grounded state
         // if (!isGrounded) return;
         // If the jump button was just pressed start jump timer
@@ -71,5 +71,27 @@ public class BlobController : MonoBehaviour
         if (elapsedTime > jumpHoldDuration || Input.GetButtonUp("Jump")) {
             isHoldingJump = false;
         }
+    }
+
+    public float gravityScale = 9.81f;
+    public float fallingGravityScale = 40f;
+    // Use Our own custom gravity system with increased gravity on fall down
+    // This system automatically turns off if the rigid body has `Use Gravity` turned on
+    void FastFallGravitySystem() {
+        // exit if regular unity gravity is turned on
+        if (rb.useGravity) return;
+
+        // set gravity for this frame
+        float thisFrameGravityScale;
+        if (rb.velocity.y >= 0) {
+            // if going up, use small gravity
+            thisFrameGravityScale = gravityScale;
+        } else {
+            // if going down use high gravity
+            thisFrameGravityScale = fallingGravityScale;
+        }
+
+        // apply gravity which we just set
+        rb.velocity -= thisFrameGravityScale * Time.deltaTime * Vector3.up;
     }
 }
